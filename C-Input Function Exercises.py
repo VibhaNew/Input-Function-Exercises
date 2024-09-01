c4821dbb962dae0952eb55a4d579409b90c6f661
MyPyCo <vibhavari.kulkarni@gmail.com>
2024-09-01 09:35:49 +0530

    Add files via upload

---
+++ C-Input Function Exercises.py
@@ -0,0 +1,8 @@
+#differnet ways input function can be used to take input and print in between the strings.
+# Method 1:using a + sign
+print("Hello "+input("What is your name?")+"!")
+#Method 2:using Comma instead of a + sign
+print("Hello",input("What's your name? "),"!")
+# creating a seperate Variable Name1 for Input and using it in a string to print
+Name1=input("Your name: ")
+print("Hello", Name1,"!")
